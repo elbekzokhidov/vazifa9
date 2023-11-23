36e1aa734ad31ea93297c9a6e26ca7830743a97a
elbekzokhidov <zokhidov@gmail.com>
2023-11-23 20:33:05 +0300

    vazifa9

---
+++ app.js
@@ -0,0 +1,168 @@
+// 51-masala
+// let arr = [1,2,3,4,5]
+// for(let i = arr[0]; i <= arr.length; i++){
+
+// }
+// 52-masala
+// Biz hali Object mavzusini o'tmdik
+
+// 53-masala
+// Bu masalaga chunmadm
+
+// 54-masala
+// Bu masalaga oid method o'tmadik
+
+// 55-masala
+/*function daraja(x, y) {
+ return x ** y
+}    
+console.log(daraja(5,2));*/
+
+// 56-masala
+/*let arr = [1, 2, 3, 4];
+function songiElement(arr) {
+    return arr
+}
+console.log(arr[arr.length - 1]);*/
+
+// 57-masala
+/*function kabisa(yil) {
+if ( yil % 4 == 0 || yil % 100 == 0 || yil % 400 == 0){
+    return true
+} else {
+    return false
+}
+}    
+console.log(kabisa(1968));*/
+
+// 58-masala
+// Bu masalaga oid method o'tmadik
+
+// 59-masala
+/*function teskariBool(bool) {
+return !bool
+}    
+console.log(teskariBool(true));*/
+
+// 60-masala
+/*function juftMiToqmi(son) {
+    if(son % 2 == 0){
+        return "Juft"
+    } else {
+        return " Toq"
+    }
+}
+console.log(juftMiToqmi(6));*/
+
+// 61-masala
+/*let quti  =  (a) =>{
+    return a * 2 + 1
+}
+console.log(quti(3));*/
+
+// 62-masala
+/*let arr = [1,2,4,6,]
+function arrayToString(arr) {
+    return arr
+}
+console.log(arr.toString())*/
+
+// 63-masala
+/*const arr1 = [1,2,3,5];
+const arr2  = [salom,5,xayr,7,9]
+function birlash(arr1, arr2){
+}
+console.log(arr1.concat(arr2));*/
+
+// 64-masala
+// Bu masalaga oid method o'tmadik
+
+// 65-masala
+/*function arrElement(arr, index) {
+return arr[index]
+}
+console.log(arrElement([1,2,4,6,7,33],4/2));*/
+
+// 66-masala
+/*let yegini = (arr) => {
+
+}
+let arr;
+for(let i = 0; i <= arr.length; i++){
+}
+console.log(yegini([1,2,3,4,5]));*/
+
+// 67-masala
+/*let son  = (a) => {
+    if(a == "bir"){
+        return "1"
+    } else if(a == "ikki"){
+        return "2"
+    }else if(a == "uch"){
+        return "3"
+    }else if(a == "tort"){
+        return "4"
+    }else if(a == "besh"){
+        return "5"
+    }else if(a == "olti"){
+        return "6"
+    }else if(a == "yetti"){
+        return "7"
+    }else if(a == "sakkiz"){
+        return "8"
+    }else if(a == "toqqiz" ){
+        return "9"
+    } else if(a == "nol"){
+        return "0"
+    } else{
+        return "0 dan to'qqizgacha raqam kititing!"
+    }
+}
+console.log(son("bir"));*/
+
+// 68-masala
+// Bu masalaga oid method o'tmadik
+
+// 69-masala
+// Bu masalaaga oid method otmadik
+// biz faqat pop, push, shift, unshift, join, split, length methodlarini o'tdik holos
+
+// 70-masala
+// Bu masalaga tushunmadm
+
+// 71-masala
+/*function sayohat(odamlar){
+   return odamlar / 5
+}
+console.log(sayohat(30));*/
+
+// 72-masala
+// Bu masalaga oid method o'tmadik
+
+// 73-masala
+// Biz hali object mavzusini o'tmadik
+
+// 74-masala
+// Bu masalaga oid method otmadik
+
+// 75-masala
+// Bu masalaga oid method otmadik
+
+// 76-masala
+// Bu masalaga oid method otmadik
+
+// 77-masala
+// Bu masalaga tushunmadm 
+// Koplik deb nmani nazarda tutyapti?
+
+// 78-masala
+// Bu masalaga oid method otmadik
+
+// 79-masala
+// Bu masalaga oid method otmadik
+
+// 80-masala
+/*let ahvol = (a) => {
+    return "Today, I am feeling " + a
+}
+console.log(ahvol("sad"));*/
